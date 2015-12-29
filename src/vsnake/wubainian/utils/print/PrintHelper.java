14915ef5d36cda2757d23fdbb32e845beea2be50
guoliang <guoliang@tigerjoys.com>
2015-12-29 18:11:57 +0800

    add log, stage commit

--- src/vsnake/wubainian/utils/print/PrintHelper.java
+++ src/vsnake/wubainian/utils/print/PrintHelper.java
@@ -5,61 +5,66 @@ import vsnake.wubainian.utils.CommonConst;
 
 public class PrintHelper {
 	//base
-	public void log(String msg, byte t){
-		Log.d(CommonConst.LOG_TAG_JAVA, msg + " = " + t);
+	public static void log(String msg, byte t){
+		Log.e(CommonConst.LOG_TAG_JAVA, msg + " = " + t);
 	} 
-	public void log(String msg, short t){
-		Log.d(CommonConst.LOG_TAG_JAVA, msg + " = " + t);
+	public static void log(String msg, short t){
+		Log.e(CommonConst.LOG_TAG_JAVA, msg + " = " + t);
 	} 
-	public void log(String msg, int t){
-		Log.d(CommonConst.LOG_TAG_JAVA, msg + " = " + t);
+	public static void log(String msg, int t){
+		Log.e(CommonConst.LOG_TAG_JAVA, msg + " = " + t);
 	} 
-	public void log(String msg, long t){
-		Log.d(CommonConst.LOG_TAG_JAVA, msg + " = " + t);
+	public static void log(String msg, long t){
+		Log.e(CommonConst.LOG_TAG_JAVA, msg + " = " + t);
 	} 
-	public void log(String msg, float t){
-		Log.d(CommonConst.LOG_TAG_JAVA, msg + " = " + t);
+	public static void log(String msg, float t){
+		Log.e(CommonConst.LOG_TAG_JAVA, msg + " = " + t);
 	} 
-	public void log(String msg, double t){
-		Log.d(CommonConst.LOG_TAG_JAVA, msg + " = " + t);
+	public static void log(String msg, double t){
+		Log.e(CommonConst.LOG_TAG_JAVA, msg + " = " + t);
 	} 
-	public void log(String msg, boolean t){
-		Log.d(CommonConst.LOG_TAG_JAVA, msg + " = " + t);
+	public static void log(String msg, boolean t){
+		Log.e(CommonConst.LOG_TAG_JAVA, msg + " = " + t);
 	} 
-	public void log(String msg, char t){
-		Log.d(CommonConst.LOG_TAG_JAVA, msg + " = " + t);
+	public static void log(String msg, char t){
+		Log.e(CommonConst.LOG_TAG_JAVA, msg + " = " + t);
 	}
-	public void log(String msg, String t){
-		Log.d(CommonConst.LOG_TAG_JAVA, msg + " = " + t);
+	public static void log(String msg, String t){
+		Log.e(CommonConst.LOG_TAG_JAVA, msg + " = " + t);
 	}
 	
 	
 	//array 
-	public void log(String msg, byte[] t){
-		Log.d(CommonConst.LOG_TAG_JAVA, msg + " = " + PrintDetail.getStr(t));
+	public static void log(String msg, byte[] t){
+		Log.e(CommonConst.LOG_TAG_JAVA, msg + " = " + PrintDetail.getStr(t));
 	}
-	public void log(String msg, short[] t){
-		Log.d(CommonConst.LOG_TAG_JAVA, msg + " = " + PrintDetail.getStr(t));
+	public static void log(String msg, short[] t){
+		Log.e(CommonConst.LOG_TAG_JAVA, msg + " = " + PrintDetail.getStr(t));
 	}
-	public void log(String msg, int[] t){
-		Log.d(CommonConst.LOG_TAG_JAVA, msg + " = " + PrintDetail.getStr(t));
+	public static void log(String msg, int[] t){
+		Log.e(CommonConst.LOG_TAG_JAVA, msg + " = " + PrintDetail.getStr(t));
 	}
-	public void log(String msg, long[] t){
-		Log.d(CommonConst.LOG_TAG_JAVA, msg + " = " + PrintDetail.getStr(t));
+	public static void log(String msg, long[] t){
+		Log.e(CommonConst.LOG_TAG_JAVA, msg + " = " + PrintDetail.getStr(t));
 	}
-	public void log(String msg, float[] t){
-		Log.d(CommonConst.LOG_TAG_JAVA, msg + " = " + PrintDetail.getStr(t));
+	public static void log(String msg, float[] t){
+		Log.e(CommonConst.LOG_TAG_JAVA, msg + " = " + PrintDetail.getStr(t));
 	}
-	public void log(String msg, double[] t){
-		Log.d(CommonConst.LOG_TAG_JAVA, msg + " = " + PrintDetail.getStr(t));
+	public static void log(String msg, double[] t){
+		Log.e(CommonConst.LOG_TAG_JAVA, msg + " = " + PrintDetail.getStr(t));
 	}
-	public void log(String msg, boolean[] t){
-		Log.d(CommonConst.LOG_TAG_JAVA, msg + " = " + PrintDetail.getStr(t));
+	public static void log(String msg, boolean[] t){
+		Log.e(CommonConst.LOG_TAG_JAVA, msg + " = " + PrintDetail.getStr(t));
 	}
-	public void log(String msg, char[] t){
-		Log.d(CommonConst.LOG_TAG_JAVA, msg + " = " + PrintDetail.getStr(t));
+	public static void log(String msg, char[] t){
+		Log.e(CommonConst.LOG_TAG_JAVA, msg + " = " + PrintDetail.getStr(t));
 	}
-	public void log(String msg, String[] t){
-		Log.d(CommonConst.LOG_TAG_JAVA, msg + " = " + PrintDetail.getStr(t));
+	public static void log(String msg, String[] t){
+		Log.e(CommonConst.LOG_TAG_JAVA, msg + " = " + PrintDetail.getStr(t));
+	} 
+	
+	//special
+	public static void log_call(String t){
+		Log.e(CommonConst.LOG_TAG_JAVA,  t + " = " + "is called");
 	} 
 }
